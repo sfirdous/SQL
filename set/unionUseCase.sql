07961e306824d067fbfd7772d8e9cb7d7f6487f1
firdous <sfirdous0404@gmail.com>
2025-06-11 11:14:01 +0530

    union use case

---
+++ set/unionUseCase.sql
@@ -0,0 +1,32 @@
+use SalesDB
+
+--Union to combine data from different table's having similar information
+SELECT 'Orders' AS SourceTable   --Add a static column that indicated data belongs to which table
+       ,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.Orders
+UNION
+SELECT 'OrdersArchive' AS SourceTable
+       ,[OrderID]
+      ,[ProductID]
+      ,[CustomerID]
+      ,[SalesPersonID]
+      ,[OrderDate]
+      ,[ShipDate]
+      ,[OrderStatus]
+      ,[ShipAddress]
+      ,[BillAddress]
+      ,[Quantity]
+      ,[Sales]
+      ,[CreationTime]
+FROM Sales.OrdersArchive
