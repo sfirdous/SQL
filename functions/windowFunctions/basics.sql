15e8c293cf1b9fb08e6d443c62de40011647a7c5
firdous <sfirdous0404@gmail.com>
2025-06-24 21:55:08 +0530

    done with partion by and order by clause

--- functions/windowFunctions/basics.sql
+++ functions/windowFunctions/basics.sql
@@ -9,6 +9,8 @@ SUM(Sales) as productSales
 FROM Sales.Orders
 GROUP BY ProductID
 
+
+--Partiton By Clause
 /*TASK : 2
 Find total Sales for each product additionally provide details such as order id  and order date*/
 
@@ -16,7 +18,26 @@ SELECT
 OrderID,
 OrderDate,
 ProductID,
-SUM(Sales) OVER(PARTITION BY ProductID) as totalProductSales
+Sales,
+SUM(Sales) OVER(PARTITION BY ProductID) as TotalProductSales,
+SUM(Sales) OVER() as TotalSales
+FROM Sales.Orders
+
+/*Task : 3 Find the total sales for each combination of product and order status*/
+SELECT 
+OrderId,
+ProductID,
+Sales,
+OrderStatus,
+SUM(Sales) OVER (PARTITION BY ProductID,OrderStatus) as SalesByProductsAndStatus
 FROM Sales.Orders
 
 
+--Order By
+/*TASK : 4
+Rank each order based on their sales from highest to lowest*/
+SELECT 
+OrderID,
+Sales,
+RANK() OVER(ORDER BY Sales DESC ) as RankSales
+FROM Sales.Orders
